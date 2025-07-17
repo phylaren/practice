4d67f0c7c1f7721dc339ac16b3b7c68d209a1ec1
phylaren <julboichuk@gmail.com>
2025-07-17 17:37:58 +0300

    shopping list

--- js/script.js
+++ js/script.js
@@ -1,34 +1,18 @@
-let numbers = [
-    "Jenny:123123123",
-    "Vicky:907813459",
-    "Jasmine:800789567",
-    "Josh: 456724085",
-    "Jasmine:234123867",
-]
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const list = document.querySelector("ul");
 
-let input = document.querySelector("input");
-let btn = document.querySelector("button");
-let para = document.querySelector("p");
+btn.addEventListener("click", () => {
+  let item = document.createElement("li");
+  
+  input.focus();
+  item.textContent = input.value;
+  input.value = "";
 
-btn.addEventListener("click", findNumber);
-
-function findNumber(){
-    para.textContent = "Result: "
-    let searchedHuman = input.value;
-    input.value = "";
-    input.focus();
-
-    for(let i =0; i<numbers.length; i++){
-        let contact = numbers[i].split(":");
-        console.log(contact)
-        if(contact[0]===searchedHuman){
-            para.textContent += `
-                ${contact[0]} number is ${contact[1]}
-            `;
-            break;
-        }
-    }
-    if(para.textContent === "Result: "){
-        para.textContent += "Not found";
-    }
-}
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "delete";
+  deleteBtn.addEventListener("click", () => item.remove());
+  item.appendChild(deleteBtn);
+  
+  list.appendChild(item);
+});
