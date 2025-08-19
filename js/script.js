d3daa087c270621dde5092121fbed66dc233b58d
phylaren <julboichuk@gmail.com>
2025-08-19 10:36:47 +0300

    Postcard

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
 
-
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector(".container");
-
-    container.addEventListener("click", (e) => {
-        if(e.target.getAttribute("data-state") === "closed"){
-            e.target.setAttribute("data-state", "open");
-        }else {
-            e.target.setAttribute("data-state", "closed");
-        }
-    }); 
-});
